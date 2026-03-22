991dc71d11d203b7fcf66a7cb337cd21df01c0e8
Cetinkoenen <info.koenen@gmail.com>
2026-03-22 19:24:14 +0100

    Add home route to fix blank page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { PropertyDetailPage } from "./features/property-detail";
 
+function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>App läuft 🚀</h1>
+      <p>Gehe zu /objekte/1 um ein Objekt zu sehen.</p>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/objekte/:propertyId" element={<PropertyDetailPage />} />
     </Routes>
   );
